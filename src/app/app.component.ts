7855c0e03b9fd32676e840dd504f85162ef80460
brampeirs <peirsbram1@gmail.com>
2019-01-10 13:49:56 +0100

    local functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
 import { Component } from '@angular/core';
+import { animate, keyframes, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('moveInLeft', [
+      transition('void=> *', [style({transform: 'translateX(300px)'}),
+        animate(200, keyframes([
+          style({transform: 'translateX(300px)'}),
+          style({transform: 'translateX(0)'})
+
+        ]))]),
+      transition('*=>void', [style({transform: 'translateX(0px)'}),
+        animate(100, keyframes([
+          style({transform: 'translateX(0px)'}),
+          style({transform: 'translateX(300px)'})
+
+        ]))])
+
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'todo-app';
+  todoArray = [];
+  todo;
+
+  //todoForm: new FormGroup()
+  addTodo(value) {
+    if (value !== '') {
+      this.todoArray.push(value);
+      //console.log(this.todos)
+    } else {
+      alert('Field required **');
+    }
+
+  }
+
+  /*delete item*/
+  deleteItem(todo) {
+    for (let i = 0; i <= this.todoArray.length; i++) {
+      if (todo == this.todoArray[i]) {
+        this.todoArray.splice(i, 1);
+      }
+    }
+  }
+
+// submit Form
+  todoSubmit(value: any) {
+    if (value !== '') {
+      this.todoArray.push(value.todo);
+      //this.todoForm.reset()
+    } else {
+      alert('Field required **');
+    }
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
